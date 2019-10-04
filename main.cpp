0080824fa8ea393468d4466de30ac9fc8d7f1ed6
rcaina <renzini2018@gmail.com>
2019-10-04 12:18:10 -0600

    copy of lab 1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "Lexer.h"
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+	Lexer activate(argv[1]);
+        
+	
+	return 0;
+}
